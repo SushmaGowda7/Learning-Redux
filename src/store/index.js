ea19b764528eb0e3ee477d5add3ff861f526bdcb
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2022-10-21 21:48:36 +0530

    use of auth reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,49 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
-    if(action.type === 'increment'){
-<<<<<<< HEAD
-        return { counter: state.counter + 1 }
-    }else {
-        if(action.type === 'decrement'){
-            return { counter: state.counter - 1 }
-=======
-        return { counter: state.counter + 5 }
-    }else {
-        if(action.type === 'decrement'){
-            return { counter: state.counter - 5 }
->>>>>>> fcd8e7c67f40bfd6fa9d23823f02a6f9bd0829ee
-    }
-}
-    return state;
-};
+const initialCounterState = {counter: 0, showCounter: true}
 
-const store = createStore(counterReducer);
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialCounterState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },increase(state, action) {
+            state.counter+= action.payload;
+        },
+        toggleCounter(state) {
+            state.showCounter= !state.showCounter;
+        }
+    }
+});
+
+const iniialAuthState = {isAuthenticated: false}
+
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: iniialAuthState,
+    reducers: {
+        login(state){
+            state.isAuthenticated = true;
+        },
+        logout(state){
+            state.isAuthenticated = false;
+        }
+    }
+});
+
+
+const store = configureStore({
+    reducer: {
+        counter: counterSlice.reducer, auth: authSlice.reducer
+    }
+});
+
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
